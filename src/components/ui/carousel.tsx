6533bc35986843954c1765da9a51300a201e2e7b
Zulhairy <zulhairy1558@gmail.com>
2026-08-08 22:02:30 +0800

    Fix lint errors

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -95,7 +95,8 @@ function Carousel({
 
   React.useEffect(() => {
     if (!api) return
-    onSelect(api)
+    // Defer to avoid synchronous setState in effect (lint: react-hooks/set-state-in-effect)
+    queueMicrotask(() => onSelect(api))
     api.on("reInit", onSelect)
     api.on("select", onSelect)
 
